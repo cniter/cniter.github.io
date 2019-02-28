12acc23f97466af6019db3a9151593c97fddd2ce
Shaun Yuan <shaunyuan@foxmail.com>
2019-02-28 18:56:11 +0800

    Site updated: 2019-02-28 18:55:56

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-import resetLabelStyle from"./partial/label.js";import articleShowMore from"./partial/article.js";$(document).ready(function(){resetLabelStyle(),articleShowMore()});
+import resetLabelStyle from"./partial/label.js";import articleShowMore from"./partial/article.js";import copyClipboard from"./widget/copy.js";$(document).ready(function(){resetLabelStyle(),articleShowMore(),copyClipboard()});
